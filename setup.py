10176807570579d1a54aa2ae478c14297b1f2cb6
africa itcase <africa@faunris-work>
2014-06-30 13:52:32 +0600

    initial project structure

---
+++ setup.py
@@ -0,0 +1,44 @@
+from unisender import __version__
+from setuptools import setup
+
+setup(
+    name='unisender',
+    version=__version__,
+    url='http://github.com/ITCase/unisender/',
+    author='Efimov Alexey',
+    author_email='alexey.efimov@itcase.pro',
+
+    packages=['django-unisender', ],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="nose.collector",
+    license="MIT",
+    description='Django admin unisender integration',
+    package_data={
+        '': ['*.txt', '*.rst', '*.md'],
+    },
+    long_description="http://github.com/ITCase/unisender/",
+    install_requires=[
+        "pyunisend",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Django",
+        "Topic :: Internet",
+        "Topic :: Email",
+        "License :: Repoze Public License",
+    ],
+)
