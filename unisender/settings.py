5a2e8bb479740a234e67f27ab1f9d9bdf492d872
africa itcase <africa@faunris-work>
2014-07-08 15:47:34 +0600

    fix readme; del POST method from settings

--- unisender/settings.py
+++ unisender/settings.py
@@ -3,6 +3,4 @@ from django.conf import settings
 
 UNISENDER_API_KEY = getattr(settings, 'UNISENDER_API_KEY', None)
 
-UNISENDER_METHOD = getattr(settings, 'UNISENDER_METHOD', 'POST')
-
 UNISENDER_TEST_MODE = getattr(settings, 'UNISENDER_TEST_MODE', False)
